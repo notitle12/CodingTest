30be2965d9d96925b6d5c3592bd888fae9a77fed
Notitle <133619212+notitle12@users.noreply.github.com>
2024-11-17 22:02:53 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        char[] my_string_char = my_string.toCharArray();
-        char[] invert_char = new char[my_string.length()];
-        for(int i = 0; i < my_string.length(); i++) {
-            invert_char[i] = my_string_char[my_string.length() - i - 1];
-        }
-        String answer = new String(invert_char);
-        return answer;
+        return new StringBuilder(my_string).reverse().toString();
     }
 }
