e0396f0acbf5d9ceb4641b350bba80b1ff788194
HUISU <133619212+notitle12@users.noreply.github.com>
2025-05-15 00:27:18 +0900

    [level 0] Title: 코드 처리하기, Time: 9.56 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,19 +1,21 @@
 class Solution {
     public String solution(String code) {
         int mode = 0;
-         StringBuilder ret = new StringBuilder();
-        for(int idx = 0; idx < code.length(); idx++) {
+        StringBuilder ret = new StringBuilder();
+        
+        for (int idx = 0; idx < code.length(); idx++) {
             char ch = code.charAt(idx);
+
             if (ch == '1') {
                 mode = 1 - mode;
-            } else {
-                if (mode == 0 && idx % 2 == 0) {
-                    ret.append(ch);
-                } else if (mode == 1 && idx % 2 == 1) {
-                    ret.append(ch);
-                }
+                continue;
+            }
+
+            if (idx % 2 == mode) {
+                ret.append(ch);
             }
         }
+        
         String answer = ret.length() > 0 ? ret.toString() : "EMPTY";
         return answer;
     }
