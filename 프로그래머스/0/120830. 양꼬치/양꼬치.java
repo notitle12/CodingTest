efd9af0a823051171556cc37be93ae53f0703ca6
HUISU <133619212+notitle12@users.noreply.github.com>
2025-05-17 18:52:17 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,6 +1,11 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = (12000 * n) + (2000 * k) - (2000 * (n / 10));
+        int answer = 0;
+        int meat_price = n * 12000;
+        int drink_price = k * 2000;
+        int discount_price = 2000 * (n / 10);
+        int total = meat_price + drink_price - discount_price;
+        answer = total;
         return answer;
     }
 }
