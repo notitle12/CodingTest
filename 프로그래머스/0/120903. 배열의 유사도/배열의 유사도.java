5b3356ac445c6f5a494f11f4daaab1959d4974bc
HUISU <133619212+notitle12@users.noreply.github.com>
2025-05-17 19:13:14 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,21 +1,13 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public int solution(String[] s1, String[] s2) {
-
-        Set<String> set = new HashSet<>();
-        for (String str : s1) {
-            set.add(str);
-        }
-
-        int count = 0;
-        for (String str : s2) {
-            if (set.contains(str)) {
-                count++;
+        int answer = 0;
+        for(int i = 0; i < s1.length; i++) {
+            for(int j = 0; j < s2.length; j++) {
+                if(s1[i].equals(s2[j])) {
+                    answer += 1;
+                }
             }
         }
-
-        return count;
+        return answer;
     }
 }
