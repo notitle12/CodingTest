74570de8a8ae8e9b48e982325211e004d10666b6
HUISU <133619212+notitle12@users.noreply.github.com>
2025-05-20 01:49:32 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.16 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        if (n % 7 == 0) {
-            answer = n / 7;
-        } else {
-            answer = (n / 7) + 1; 
+        for(int i = 1; i <= n; i++) {
+            if(i * 7 / n >= 1) {
+                answer = i;
+                break;
+            }
         }
         return answer;
     }
