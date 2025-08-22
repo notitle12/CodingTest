640adedd6ffc214c3062cb7839dd839ebaa65ee2
HUISU <133619212+notitle12@users.noreply.github.com>
2025-08-23 02:19:58 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int num) {
+        int count = 0;
+        long n = num;
+
+        if (n == 1) {
+            return 0;
+        }
+
+        while (n != 1) {
+            if (count == 500) {
+                return -1;
+            }
+
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = n * 3 + 1;
+            }
+            count++;
+        }
+
+        return count;
+    }
+}
