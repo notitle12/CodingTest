a68eee055b12fca308c791b2eca32186d279e55a
HUISU <133619212+notitle12@users.noreply.github.com>
2025-08-24 23:05:42 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int len = s.length();
+        
+        if(s.length() % 2 == 0) {
+            answer = s.substring(len / 2 - 1, len / 2 + 1);
+        } else {
+            answer = s.substring(len / 2, len / 2 + 1);
+        }
+        return answer;
+    }
+}
