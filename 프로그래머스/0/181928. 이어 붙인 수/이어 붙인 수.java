13362d23f006dea53310e25b9fbcde086cd95188
HUISU <133619212+notitle12@users.noreply.github.com>
2025-09-18 19:41:46 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.07 ms, Memory: 96.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        StringBuilder odd = new StringBuilder();
+        StringBuilder even = new StringBuilder();
+        for (int n : num_list) {
+            if (n % 2 == 0) {
+                even.append(n);
+            } else {
+                odd.append(n);
+            }
+        }
+        int oddNum = Integer.parseInt(odd.toString());
+        int evenNum = Integer.parseInt(even.toString());
+        return oddNum + evenNum;
+    }
+}
