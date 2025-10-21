6b59982e6a4b23b5ed1523c1dcb3893e4ca5c251
HUISU <133619212+notitle12@users.noreply.github.com>
2025-10-21 14:46:15 +0900

    [level 0] Title: 약수 구하기, Time: 0.24 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,20 +1,22 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        ArrayList<Integer> divisors = new ArrayList<>();
-
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                divisors.add(i);
+        ArrayList<Integer> num_list = new ArrayList<>();
+        
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                num_list.add(i);
             }
         }
-
-        int[] answer = new int[divisors.size()];
-        for (int i = 0; i < divisors.size(); i++) {
-            answer[i] = divisors.get(i);
+        
+        int[] answer = new int[num_list.size()];
+        
+        for(int i = 0; i < num_list.size(); i++) {
+            answer[i] = num_list.get(i);
         }
-
+        
+        
         return answer;
     }
 }
