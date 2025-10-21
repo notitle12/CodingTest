905e9783f3a8af545ef6a00d478f0b517ef93641
HUISU <133619212+notitle12@users.noreply.github.com>
2025-10-21 16:42:39 +0900

    [level 0] Title: qr code, Time: 0.13 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int q, int r, String code) {
+        StringBuilder sb = new StringBuilder();
+        char[] ch = code.toCharArray();
+        for(int i = 0; i < ch.length; i++) {
+            if(i % q == r) {
+                sb.append(ch[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
