534eed4383a7489a75e7e51da813d2f010859a9f
HUISU <133619212+notitle12@users.noreply.github.com>
2025-11-09 19:20:00 +0900

    [level 0] Title: 배열 만들기 5, Time: 11.26 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,12 @@
+import java.util.stream.*;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        return Arrays.stream(intStrs)
+                .map(str -> str.substring(s, s + l))
+                .mapToInt(Integer::parseInt)
+                .filter(num -> num > k)
+                .toArray();
+    }
+}
