94595825af52e42a223b456472805bc1a5a92b9c
HUISU <133619212+notitle12@users.noreply.github.com>
2025-11-15 21:31:59 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.06 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] q : queries) {
+            int i = q[0];
+            int j = q[1];
+            int temp = arr[i];
+            
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        
+        return arr;
+    }
+}
