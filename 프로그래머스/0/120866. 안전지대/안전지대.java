8b0b711a877ff8570c9a4338b65bf2d70d6c2f1e
HUISU <133619212+notitle12@users.noreply.github.com>
2026-01-08 00:06:22 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int solution(int[][] board) {
+        int n = board.length;
+        boolean[][] dangerZone = new boolean[n][n];
+        
+        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    dangerZone[i][j] = true;
+                    for (int k = 0; k < 8; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        
+                        if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                            dangerZone[nx][ny] = true;
+                        }
+                    }
+                }
+            }
+        }
+        
+        int safeCount = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!dangerZone[i][j]) {
+                    safeCount++;
+                }
+            }
+        }
+        
+        return safeCount;
+    }
+}
