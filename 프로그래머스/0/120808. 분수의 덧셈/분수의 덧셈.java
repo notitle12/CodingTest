f33ac976f26e5c4a44ef472a1311c8ec4c2b0d19
HUISU <133619212+notitle12@users.noreply.github.com>
2026-01-09 23:59:40 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int[] answer = {0, 1};
-        int numer = (numer1 * denom2) + (numer2 * denom1);
-        int denom = (denom1 * denom2);
-        int x = 1;
+        int top = numer1 * denom2 + numer2 * denom1;
+        int bottom = denom1 * denom2;
 
-        for (int i = 1; i <= Math.min(numer, denom); i++) {
-            if (numer % i == 0 && denom % i == 0) {
-                x = i;
-            }
+        int gcd = getGcd(top, bottom);
+
+        return new int[]{top / gcd, bottom / gcd};
+    }
+
+    private int getGcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
         }
-        answer[0] = numer / x;
-        answer[1] = denom / x;
-        return answer;
+        return a;
     }
 }
