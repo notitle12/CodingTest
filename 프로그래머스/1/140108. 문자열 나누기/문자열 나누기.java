360db752bd4bf4855d8c679c657cb72e40285406
HUISU <133619212+notitle12@users.noreply.github.com>
2026-02-05 10:30:34 +0900

    [level 1] Title: 문자열 나누기, Time: 0.76 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int i = 0;
+        
+        while (i < s.length()) {
+            char x = s.charAt(i);
+            int countX = 0;
+            int countOther = 0;
+            
+            while (i < s.length()) {
+                if (s.charAt(i) == x) {
+                    countX++;
+                } else {
+                    countOther++;
+                }
+                
+                i++;
+                if (countX == countOther) break;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
