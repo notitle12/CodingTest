68eb86cf693ec1be0b99dd1fb249cf11ac675be8
HUISU <133619212+notitle12@users.noreply.github.com>
2026-02-18 13:41:34 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.24 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int minRow = Integer.MAX_VALUE;
+        int minCol = Integer.MAX_VALUE;
+        int maxRow = Integer.MIN_VALUE;
+        int maxCol = Integer.MIN_VALUE;
+
+        for (int r = 0; r < wallpaper.length; r++) {
+            for (int c = 0; c < wallpaper[r].length(); c++) {
+                if (wallpaper[r].charAt(c) == '#') {
+                    minRow = Math.min(minRow, r);
+                    minCol = Math.min(minCol, c);
+                    maxRow = Math.max(maxRow, r);
+                    maxCol = Math.max(maxCol, c);
+                }
+            }
+        }
+
+        return new int[]{minRow, minCol, maxRow + 1, maxCol + 1};
+    }
+}
