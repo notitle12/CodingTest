6f01e0afd9cace407fc5363a18e92b73c85efc03
HUISU <133619212+notitle12@users.noreply.github.com>
2026-03-14 23:44:19 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1584.18 ms, Memory: 132 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] elements) {
+        Set<Integer> set = new HashSet<>();
+        int n = elements.length;
+
+        for (int len = 1; len <= n; len++) {
+            for (int start = 0; start < n; start++) {
+                int sum = 0;
+                for (int i = 0; i < len; i++) {
+                    sum += elements[(start + i) % n];
+                }
+                set.add(sum);
+            }
+        }
+
+        return set.size();
+    }
+}
