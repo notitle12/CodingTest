1802ae0e1a16d721018f02063b38aef009212f32
HUISU <133619212+notitle12@users.noreply.github.com>
2026-03-17 23:34:16 +0900

    [level 2] Title: 할인 행사, Time: 108.76 ms, Memory: 136 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        Map<String, Integer> wantMap = new HashMap<>();
+        
+        for (int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
+
+        for (int i = 0; i <= discount.length - 10; i++) {
+            Map<String, Integer> discountMap = new HashMap<>();
+            
+            for (int j = i; j < i + 10; j++) {
+                discountMap.put(discount[j], discountMap.getOrDefault(discount[j], 0) + 1);
+            }
+
+            boolean isMatch = true;
+            for (String key : wantMap.keySet()) {
+                if (discountMap.getOrDefault(key, 0) < wantMap.get(key)) {
+                    isMatch = false;
+                    break;
+                }
+            }
+
+            if (isMatch) answer++;
+        }
+
+        return answer;
+    }
+}
