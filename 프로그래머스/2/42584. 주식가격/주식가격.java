cc28d92457afdb5fe2f081c7ab28b4418f843a76
HUISU <133619212+notitle12@users.noreply.github.com>
2026-04-12 23:16:21 +0900

    [level 2] Title: 주식가격, Time: 37.71 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int len = prices.length;
+        int[] answer = new int[len];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < len; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            answer[index] = len - 1 - index;
+        }
+
+        return answer;
+    }
+}
