d6690fc1d18324eea048c154dfc5e086e17fc873
HUISU <133619212+notitle12@users.noreply.github.com>
2026-04-13 23:54:33 +0900

    [level 2] Title: 땅따먹기, Time: 24.42 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    int solution(int[][] land) {
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][2] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][3]));
+            land[i][3] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][2]));
+        }
+
+        int[] lastRow = land[land.length - 1];
+        return Math.max(Math.max(lastRow[0], lastRow[1]), Math.max(lastRow[2], lastRow[3]));
+    }
+}
