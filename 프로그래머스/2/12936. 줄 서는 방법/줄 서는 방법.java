2ea8470bcfeecece8c437d4b60c184f140b42827
HUISU <133619212+notitle12@users.noreply.github.com>
2026-06-01 22:51:35 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.03 ms, Memory: 58.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        List<Integer> numbers = new ArrayList<>();
+        long factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            numbers.add(i);
+            factorial *= i;
+        }
+
+        k--; 
+
+        for (int i = 0; i < n; i++) {
+            factorial /= (n - i);
+            int index = (int) (k / factorial);
+            answer[i] = numbers.remove(index);
+            k %= factorial;
+        }
+
+        return answer;
+    }
+}
